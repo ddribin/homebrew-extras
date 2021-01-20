9e661fe97a2c8809a4626f64470de91d80a0fe04
Dave Dribin <dave-vcs@dribin.org>
2021-01-20 00:54:51 -0600

    borg-tools, restic-tools: Bump version to 202102002.

--- Formula/borg-tools.rb
+++ Formula/borg-tools.rb
@@ -4,7 +4,7 @@ class BorgTools < Formula
   desc "Dave Dribin's Borg tools"
   homepage "https://github.com/ddribin/borg-tools"
   url "https://github.com/ddribin/borg-tools/archive/4e0e5ddb3028a105841c9b8bfa6d66f9d7aeb60c.tar.gz"
-  version "202102001"
+  version "202102002"
   sha256 "62d04c4e0123234472c6a3cc555d71edd7f8491bf11471ddc880f96f49e210cb"
   license "MIT"
 
--- Formula/restic-tools.rb
+++ Formula/restic-tools.rb
@@ -4,7 +4,7 @@ class ResticTools < Formula
   desc "Dave Dribin's Restic tools"
   homepage "https://github.com/ddribin/restic-tools"
   url "https://github.com/ddribin/restic-tools/archive/e6e824cd7cc8a57f33f5518c078d8e473178eb88.tar.gz"
-  version "202102001"
+  version "202102002"
   sha256 "3731006afedb98fae1b9f974b66c54150f53cf9f07954b2c79d6ba8b2282cd59"
   license "MIT"
 
