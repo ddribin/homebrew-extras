8902151f7865e62f1d3940d3f412136e7d6c7d8f
Dave Dribin <dave-vcs@dribin.org>
2021-01-20 00:53:53 -0600

    borg-tools, restic-tools: Switch from zipball to tarball. Remove template comments for test

--- Formula/borg-tools.rb
+++ Formula/borg-tools.rb
@@ -3,9 +3,9 @@
 class BorgTools < Formula
   desc "Dave Dribin's Borg tools"
   homepage "https://github.com/ddribin/borg-tools"
-  url "https://github.com/ddribin/borg-tools/archive/4e0e5ddb3028a105841c9b8bfa6d66f9d7aeb60c.zip"
+  url "https://github.com/ddribin/borg-tools/archive/4e0e5ddb3028a105841c9b8bfa6d66f9d7aeb60c.tar.gz"
   version "202102001"
-  sha256 "a03ccc886b365481b57774a3c9b4541d7c084b549c2b34e23afb0776c1fb4fe1"
+  sha256 "62d04c4e0123234472c6a3cc555d71edd7f8491bf11471ddc880f96f49e210cb"
   license "MIT"
 
   def install
@@ -13,15 +13,5 @@ class BorgTools < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test borg-tools`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
   end
 end
--- Formula/restic-tools.rb
+++ Formula/restic-tools.rb
@@ -3,9 +3,9 @@
 class ResticTools < Formula
   desc "Dave Dribin's Restic tools"
   homepage "https://github.com/ddribin/restic-tools"
-  url "https://github.com/ddribin/restic-tools/archive/e6e824cd7cc8a57f33f5518c078d8e473178eb88.zip"
+  url "https://github.com/ddribin/restic-tools/archive/e6e824cd7cc8a57f33f5518c078d8e473178eb88.tar.gz"
   version "202102001"
-  sha256 "5ef8229933533f46bbf1c1bfb51effc09281d5e98e9a1e9d7bf3fc8233a18cc1"
+  sha256 "3731006afedb98fae1b9f974b66c54150f53cf9f07954b2c79d6ba8b2282cd59"
   license "MIT"
 
   def install
@@ -13,15 +13,6 @@ class ResticTools < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test restic-tools`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "true"
   end
 end
