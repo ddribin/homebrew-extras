717b20151f6efd9998d80665a8b7ceb79896f98f
Dave Dribin <dave-vcs@dribin.org>
2021-01-21 00:17:58 -0600

    Add Casks/borg-binary.rb for official prebuilt binary.

---
+++ Casks/borg-binary.rb
@@ -0,0 +1,13 @@
+cask "borg-binary" do
+  version "1.1.15"
+  sha256 "c28cd2e2bf8eb4f00cc058ee9e45844b547f3ee7b40e898fa452f2a41288eb02"
+
+  url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-macosx64"
+  name "Borg backup binary"
+  desc "Official prebuilt binary of Borg backup"
+  homepage "https://www.borgbackup.org"
+
+  conflicts_with formula: "borgbackup"
+
+  binary "borg-macosx64", target: "borg"
+end
