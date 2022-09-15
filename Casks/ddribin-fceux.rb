dabf2df94095168e8b3ea1adeab7bc8b67ba235c
Dave Dribin <dave-vcs@dribin.org>
2022-09-15 10:25:19 -0500

    Add Casks/ddribin-fceux.rb with version 2.6.4

---
+++ Casks/ddribin-fceux.rb
@@ -0,0 +1,13 @@
+cask "ddribin-fceux" do
+  version "2.6.4"
+  sha256 "104b61e17cbb16d345aa2df625ac14f8a7d947d768cbe6ae6150c36a7be454bd"
+
+  url "http://sourceforge.net/projects/fceultra/files/Binaries/#{version}/fceux-#{version}-Darwin.dmg/download"
+  name "FCEUX binary"
+  desc "Official FCEUX binary"
+  homepage "https://fceux.com/web/home.html"
+
+  conflicts_with formula: "fceux"
+
+  app "fceux.app"
+end
