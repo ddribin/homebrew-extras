6a805ad990730783aab192a06fef8536e4808c68
Dave Dribin <dave-vcs@dribin.org>
2023-02-05 18:07:11 -0600

    icestorm: Bump to version 2022122701

--- Formula/icestorm.rb
+++ Formula/icestorm.rb
@@ -1,10 +1,10 @@
 class Icestorm < Formula
   desc "Tools for analyzing and creating Lattice iCE40 bitstream files"
   homepage "https://www.clifford.at/icestorm/"
-  url "https://github.com/YosysHQ/icestorm/archive/9f66f9ce16941c6417813cb87653c735a78b53ae.tar.gz",
+  url "https://github.com/YosysHQ/icestorm/archive/45f5e5f3889afb07907bab439cf071478ee5a2a5.tar.gz",
       verified: "github.com/YosysHQ/icestorm/"
-  version "2022043001"
-  sha256 "91975085e49dc7ad8e600ca471ec6e4691243be78631e5f576f305700b94d605"
+  version "2022122701"
+  sha256 "c0e6aed765cf9915874d422f6aea2172878ff6b06fe95ab351de5d62fc9aa40b"
   license "ISC"
 
   depends_on "libftdi0"
