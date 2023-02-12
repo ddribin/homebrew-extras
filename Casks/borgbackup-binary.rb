e9aa1e61d72c60fe474659d7f57ca08f986d6943
Dave Dribin <dave-vcs@dribin.org>
2023-02-12 00:20:07 -0600

    borgbackup-binary: Bump to version 1.2.3

--- Casks/borgbackup-binary.rb
+++ Casks/borgbackup-binary.rb
@@ -1,6 +1,6 @@
 cask "borgbackup-binary" do
-  version "1.2.2"
-  sha256 "b045de49d99a69ba7de1ff786db335de1e71861feab4f529186d26db2d7ea4f5"
+  version "1.2.3"
+  sha256 "04bb4aeef4756641cb0cd354380b71a82a58c1fa9740afe290e34b1d9e6db958"
 
   url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-macos64",
       verified: "github.com/borgbackup/borg/"
