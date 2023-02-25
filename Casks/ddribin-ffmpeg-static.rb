db528fec415b7469ff1140b6c4524b8bece04790
Dave Dribin <dave-vcs@dribin.org>
2023-02-24 22:41:09 -0600

    ddribin-ffmpeg-static: Update to use binary from https://www.osxexperts.net, as it has both Inte and Apple Silicon binaries

--- Casks/ddribin-ffmpeg-static.rb
+++ Casks/ddribin-ffmpeg-static.rb
@@ -1,11 +1,13 @@
 cask "ddribin-ffmpeg-static" do
-  version "4.3.1"
-  sha256 "8f3e9fca63a76d9f69bc60daf5aa8b524178de0e425452a2ecd872a9fd2eb9f6"
+  arch arm: "arm", intel: "intel"
+  version "6.0"
+  sha256 arm:   "15e67ff413d3d2436ddb3efd282344e50b8f1c6f834979b984371b90ebaf0449",
+         intel: "4c3623c2356459d17375410d8b3b598b972e20c27131389d123e3f9470793dee"
 
-  url "https://evermeet.cx/ffmpeg/ffmpeg-#{version}.zip"
+  url "https://www.osxexperts.net/ffmpeg#{version.major}#{arch}.zip"
   name "ffmpeg (static build)"
-  desc "Static build of ffmpeg binary from FFmpeg"
-  homepage "https://evermeet.cx/ffmpeg/"
+  desc "Static build of ffmpeg binary from OSXExperts.net"
+  homepage "https://www.osxexperts.net"
 
   conflicts_with formula: "ffmpeg"
 
