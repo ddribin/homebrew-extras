2e95ede7fd5545c00937b27572ed1751f4d423ca
Dave Dribin <dave-vcs@dribin.org>
2023-03-17 19:15:58 -0500

    restic-tools: Bump version to 2023031701

--- Formula/restic-tools.rb
+++ Formula/restic-tools.rb
@@ -1,9 +1,9 @@
 class ResticTools < Formula
   desc "Dave Dribin's Restic tools"
   homepage "https://github.com/ddribin/restic-tools"
-  url "https://github.com/ddribin/restic-tools/archive/e6e824cd7cc8a57f33f5518c078d8e473178eb88.tar.gz"
-  version "202102002"
-  sha256 "3731006afedb98fae1b9f974b66c54150f53cf9f07954b2c79d6ba8b2282cd59"
+  url "https://github.com/ddribin/restic-tools/archive/be5e3fdd87d3048f2c9f6385829eaef87e63c83f.tar.gz"
+  version "2023031701"
+  sha256 "5501831cf264bd5b558551fcf1c92cbb0993f9d0ff38e1ebcaae9be2fc8b8260"
   license "MIT"
 
   def install
