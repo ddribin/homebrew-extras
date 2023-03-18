1933ed9e0b0f91d6803be95593374d88892406e1
Dave Dribin <dave-vcs@dribin.org>
2023-03-18 12:13:03 -0500

    restic-tools: Bump version to 2023031801 and switch to using ":git" URL

--- Formula/restic-tools.rb
+++ Formula/restic-tools.rb
@@ -1,9 +1,10 @@
 class ResticTools < Formula
   desc "Dave Dribin's Restic tools"
   homepage "https://github.com/ddribin/restic-tools"
-  url "https://github.com/ddribin/restic-tools/archive/be5e3fdd87d3048f2c9f6385829eaef87e63c83f.tar.gz"
-  version "2023031701"
-  sha256 "5501831cf264bd5b558551fcf1c92cbb0993f9d0ff38e1ebcaae9be2fc8b8260"
+  url "https://github.com/ddribin/restic-tools.git",
+    using: :git,
+    revision: "76a036fc4fb647436b24ea86c2927af3fbaeb6c3"
+  version "2023031801"
   license "MIT"
 
   def install
