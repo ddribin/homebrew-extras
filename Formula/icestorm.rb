bafc40cf7d42dab25cdd4d41171493946309f37e
Dave Dribin <dave-vcs@dribin.org>
2023-03-19 14:15:27 -0500

    icestorm: Update URL to git repo. Update Python version to 3.11

--- Formula/icestorm.rb
+++ Formula/icestorm.rb
@@ -1,14 +1,13 @@
 class Icestorm < Formula
   desc "Tools for analyzing and creating Lattice iCE40 bitstream files"
   homepage "https://www.clifford.at/icestorm/"
-  url "https://github.com/YosysHQ/icestorm/archive/45f5e5f3889afb07907bab439cf071478ee5a2a5.tar.gz",
-      verified: "github.com/YosysHQ/icestorm/"
-  version "2022122701"
-  sha256 "c0e6aed765cf9915874d422f6aea2172878ff6b06fe95ab351de5d62fc9aa40b"
+  url "https://github.com/YosysHQ/icestorm.git",
+    revision: "45f5e5f3889afb07907bab439cf071478ee5a2a5"
+  version "2022122702"
   license "ISC"
 
   depends_on "libftdi0"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
   depends_on "yosys"
 
   def install
