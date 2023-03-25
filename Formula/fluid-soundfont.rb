08d4632075473dad9deda8c621599c18f951773a
Dave Dribin <dave-vcs@dribin.org>
2023-03-25 13:30:49 -0500

    fluid-soundfont: Switch URL and homepage to my own GitHub mirror

--- Formula/fluid-soundfont.rb
+++ Formula/fluid-soundfont.rb
@@ -1,7 +1,7 @@
 class FluidSoundfont < Formula
   desc "Fluild Soundfont"
-  homepage "https://packages.debian.org/source/stable/fluid-soundfont"
-  url "http://deb.debian.org/debian/pool/main/f/fluid-soundfont/fluid-soundfont_3.1.orig.tar.gz"
+  homepage "https://github.com/ddribin/fluid-soundfont"
+  url "https://github.com/ddribin/fluid-soundfont/releases/download/v3.1/fluid-soundfont_3.1.orig.tar.gz"
   sha256 "2621acaa1c78e4abdb24bdd163230cc577e61276936d6aa6e3180582142f0343"
 
   def install
