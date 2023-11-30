79aa603925cfa377d2d8c5afbdaaa21ae3bb3415
Dave Dribin <dave-vcs@dribin.org>
2023-11-30 10:25:09 -0600

    llvm-mos: Fix verified URL

--- Casks/llvm-mos.rb
+++ Casks/llvm-mos.rb
@@ -3,15 +3,16 @@ cask "llvm-mos" do
   sha256 "e8932a1d3cb586f0ce2b7e9982bb8de3ebddfc72f7669422ed0739bd65825843"
 
   url "https://github.com/llvm-mos/llvm-mos-sdk/releases/download/v#{version}/llvm-mos-macos.tar.xz",
-      verified: "github.com/llvm-mos/llvm-mos-sdk"
+      verified: "github.com/llvm-mos/llvm-mos-sdk/"
   name "LLVM-MOS"
-  desc "Compiler toolchain and platform libraries for 6502 targets"
+  desc "Compiler toolchain and platform SDK libraries for MOS 6502 targets"
   homepage "https://www.llvm-mos.org/"
 
   artifact "llvm-mos", target: "#{HOMEBREW_PREFIX}/llvm-mos"
 
   caveats <<~EOS
-    LLVM-MOS will be installed to #{HOMEBREW_PREFIX}/llbm-mos.
+    LLVM-MOS files will be installed to:
+      #{HOMEBREW_PREFIX}/llbm-mos.
 
     WARNING: Don't install LLVM-MOS into your path if you already have
     LLVM/Clang installed. LLVM-MOS conflicts with other LLVM/Clang
