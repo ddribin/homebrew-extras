9818f20464c7aee59db2cbc34a7bc7b36e06cb92
Dave Dribin <dave-vcs@dribin.org>
2024-01-18 00:00:23 -0600

    borg-tools: Bump to version 2024011701

--- Formula/borg-tools.rb
+++ Formula/borg-tools.rb
@@ -3,8 +3,8 @@ class BorgTools < Formula
   homepage "https://github.com/ddribin/borg-tools"
   url "https://github.com/ddribin/borg-tools.git",
     using:    :git,
-    revision: "9dba274ecd65f772940933b5a89bcb258a3c8300"
-  version "2023031801"
+    revision: "f8069d7ef320c29055cb71fed413c463207e7f7c"
+  version "2024011701"
   license "MIT"
 
   def install
