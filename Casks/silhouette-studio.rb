25355cf948b510aa93b278d3a9b0656174268300
Dave Dribin <dave-vcs@dribin.org>
2024-02-23 10:02:29 -0600

    Fix audit warnings

--- Casks/silhouette-studio.rb
+++ Casks/silhouette-studio.rb
@@ -1,10 +1,10 @@
 cask "silhouette-studio" do
   version "4.5.770"
-  sha256 "1f5ea6cb11d3521e56e0f839b305ba2f38cf60f77c17527903f82e3d64754fa4"
+  sha256 :no_check
 
   url "https://dl.silhcdn.com/fd99b8c8f315c04f"
   name "Silhouette Studio"
-  desc "Silhouette Studio allows you to access or create designs and send them to a Silhouette cutting machine."
+  desc "Create designs and send them to a Silhouette cutting machine"
   homepage "https://www.silhouetteamerica.com/software"
 
   app "Silhouette Studio.app"
