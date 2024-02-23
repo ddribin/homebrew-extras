1e4e0991e8056d7ad7e033477910f5cac7a29b1f
Dave Dribin <dave-vcs@dribin.org>
2024-02-23 10:09:26 -0600

    Fix another test-bot warning. Upgrade to GitHub to actions/checkout@v4

--- Casks/silhouette-studio.rb
+++ Casks/silhouette-studio.rb
@@ -2,7 +2,8 @@ cask "silhouette-studio" do
   version "4.5.770"
   sha256 :no_check
 
-  url "https://dl.silhcdn.com/fd99b8c8f315c04f"
+  url "https://dl.silhcdn.com/fd99b8c8f315c04f",
+    verified: "https://dl.silhcdn.com/"
   name "Silhouette Studio"
   desc "Create designs and send them to a Silhouette cutting machine"
   homepage "https://www.silhouetteamerica.com/software"
