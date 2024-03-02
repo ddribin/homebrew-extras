5d907cf28c102a7416fccd32755d4f9f4833bbe4
Dave Dribin <dave-vcs@dribin.org>
2024-03-02 16:07:49 -0600

    ddribin-ffmpeg-static: Bump version to 6.1.1 (Arm) and 6.1 (Intel)

--- Casks/ddribin-ffmpeg-static.rb
+++ Casks/ddribin-ffmpeg-static.rb
@@ -1,11 +1,16 @@
 cask "ddribin-ffmpeg-static" do
   arch arm: "arm", intel: "intel"
 
-  version "6.0"
-  sha256 arm:   "15e67ff413d3d2436ddb3efd282344e50b8f1c6f834979b984371b90ebaf0449",
-         intel: "034a8d6eafb2736711d98e897dc22cc5769c7c31bde9a40ac20f58cbd57f68d8"
+  on_arm do
+    version "6.1.1"
+    sha256 "1c69a9a4a5d0172d9aec611f19d282b38b3c3d69832621ecef87926335f62e0a"
+  end
+  on_intel do
+    version "6.1"
+    sha256 "9a7de6ea24052166340c50e00dd493da49315431e6bfde5ed2a600eab9f27b65"
+  end
 
-  url "https://www.osxexperts.net/ffmpeg#{version.major}#{arch}.zip"
+  url "https://www.osxexperts.net/ffmpeg#{version.delete(".")}#{arch}.zip"
   name "ffmpeg (static build)"
   desc "Static build of ffmpeg binary from OSXExperts.net"
   homepage "https://www.osxexperts.net/"
