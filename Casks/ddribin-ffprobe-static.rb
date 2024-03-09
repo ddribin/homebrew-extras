26bdeaf2b64439664310b09565b583b5066f7892
Dave Dribin <dave-vcs@dribin.org>
2024-03-09 00:27:56 -0600

    ddribin-ffprobe-static: Bump version to 6.1.1 (Arm) and 6.1 (Intel)
    Update to use binary from https://www.osxexperts.net, as it has both Intel and Apple Silicon binaries

--- Casks/ddribin-ffprobe-static.rb
+++ Casks/ddribin-ffprobe-static.rb
@@ -1,11 +1,19 @@
 cask "ddribin-ffprobe-static" do
-  version "4.3.1"
-  sha256 "7a028fff382a722b39a0fd46d75e9caf8a49889aacc29e52f79274d204155869"
+  arch arm: "arm", intel: "intel"
 
-  url "https://evermeet.cx/ffmpeg/ffprobe-#{version}.zip"
+  on_arm do
+    version "6.1.1"
+    sha256 "8ec78ce09c0b474ae7583908c8d79d801f754d8273dca3d5a5de51f307517fc7"
+  end
+  on_intel do
+    version "6.1"
+    sha256 "977381d8cec9c477fef4cdbb259addc7e5506e47fa61087e620d5ed01a7c7a51"
+  end
+
+  url "https://www.osxexperts.net/ffprobe#{version.delete(".")}#{arch}.zip"
   name "ffprobe (static build)"
-  desc "Static build of ffprobe binary from FFmpeg"
-  homepage "https://evermeet.cx/ffmpeg/"
+  desc "Static build of ffprobe binary from OSXExperts.net"
+  homepage "https://www.osxexperts.net/"
 
   conflicts_with formula: "ffmpeg"
 
