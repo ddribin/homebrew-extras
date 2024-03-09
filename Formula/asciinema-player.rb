bb94ba1363d584aa2d3f77a8e35f47cd996f515a
Dave Dribin <dave-vcs@dribin.org>
2024-03-09 01:37:03 -0600

    asciinema-player: Bump to version 3.7.0

--- Formula/asciinema-player.rb
+++ Formula/asciinema-player.rb
@@ -1,13 +1,13 @@
 class AsciinemaPlayer < Formula
   desc "Web player for terminal sessions recorded with asciinema"
   homepage "https://github.com/asciinema/asciinema-player"
-  url "https://github.com/asciinema/asciinema-player/releases/download/v3.6.3/asciinema-player.min.js"
-  sha256 "f20f326fcc178e20412af6ab3f0bbbbafba428dc07c65bc86cc08d18e1ac7a95"
+  url "https://github.com/asciinema/asciinema-player/releases/download/v3.7.0/asciinema-player.min.js"
+  sha256 "626728ffe1629c3af960ead5ef67a1725a69e1a9ab7338e8e445378dfaffcf1b"
   license "Apache-2.0"
 
   resource "css" do
-    url "https://github.com/asciinema/asciinema-player/releases/download/v3.6.3/asciinema-player.css"
-    sha256 "18c221bb91e76f6caf136029dbd10f0539dcad1e39ad957874527aed5637ee86"
+    url "https://github.com/asciinema/asciinema-player/releases/download/v3.7.0/asciinema-player.css"
+    sha256 "19931e645146bcfe4632aaa1e75ea68c929f41a889e9b2f7f1b49839791aa217"
   end
 
   def install
