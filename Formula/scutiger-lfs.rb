2d2b5bce8fbdacf0d1a69bb40a2649237682695d
Dave Dribin <dave-vcs@dribin.org>
2024-04-07 16:35:41 -0500

    scutiger-lfs: Fix test-bot issues

--- Formula/scutiger-lfs.rb
+++ Formula/scutiger-lfs.rb
@@ -8,8 +8,8 @@ class ScutigerLfs < Formula
   bottle do
     rebuild 1
     root_url "https://www.dribin.org/dave/resources/homebrew"
-    sha256 cellar: :any_skip_relocation, sonoma: "c140c7c4bb09bf8cf1ecb70e026227c18f16f88a27b6c104b587135714da3da4"
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "5c0090f71adfd1d65f8a2d4cb946b6fadfbc8e7e1f05aaaa66ac3f33dae5a811"
+    sha256 cellar: :any_skip_relocation, sonoma:       "c140c7c4bb09bf8cf1ecb70e026227c18f16f88a27b6c104b587135714da3da4"
   end
 
   depends_on "rust" => :build
