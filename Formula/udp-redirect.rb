e40abfb74a978e24b17a9545962a3a9e6a6ec5cd
Dave Dribin <dave-vcs@dribin.org>
2024-04-22 23:06:12 -0500

    udp-redirect: Fix some test-bot warnings

--- Formula/udp-redirect.rb
+++ Formula/udp-redirect.rb
@@ -1,5 +1,5 @@
 class UdpRedirect < Formula
-  desc "A simple yet flexible and very fast UDP redirector."
+  desc "Simple yet flexible and very fast UDP redirector"
   homepage "https://github.com/danpodeanu/udp-redirect"
   head "https://github.com/danpodeanu/udp-redirect.git", branch: "main"
 
