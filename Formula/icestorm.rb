cc2bae769ee7ad0fe597a1363b7b3b49cba0df6b
Dave Dribin <dave-vcs@dribin.org>
2024-10-04 21:10:49 -0500

    icestorm: Bump to version 2024062401
    - And commit 738af822905fdcf0466e9dd784b9ae4b0b34987f.
    - Replace libftdi0 dependency with libftdi.
    - Update python@3.12 -> python@3.12.

--- Formula/icestorm.rb
+++ Formula/icestorm.rb
@@ -2,12 +2,12 @@ class Icestorm < Formula
   desc "Tools for analyzing and creating Lattice iCE40 bitstream files"
   homepage "https://www.clifford.at/icestorm/"
   url "https://github.com/YosysHQ/icestorm.git",
-    revision: "45f5e5f3889afb07907bab439cf071478ee5a2a5"
-  version "2022122702"
+    revision: "738af822905fdcf0466e9dd784b9ae4b0b34987f"
+  version "2024062401"
   license "ISC"
 
-  depends_on "libftdi0"
-  depends_on "python@3.11"
+  depends_on "libftdi"
+  depends_on "python@3.12"
   depends_on "yosys"
 
   def install
