0f7acb7623823aed80d40e876bb5201306844867
Dave Dribin <dave-vcs@dribin.org>
2025-02-21 10:04:09 -0600

    ddribin-kicad@8: Add to repository from KiCad 8.0.9 version of the cask

---
+++ Casks/ddribin-kicad@8.rb
@@ -0,0 +1,37 @@
+cask "ddribin-kicad@8" do
+  version "8.0.9"
+  sha256 "865a96ff02e9dc4d5e6d3554d8224b7d780aae7b2975329d911dfe57820ba07a"
+
+  url "https://github.com/KiCad/kicad-source-mirror/releases/download/#{version}/kicad-unified-universal-#{version}.dmg",
+      verified: "github.com/KiCad/kicad-source-mirror/"
+  name "KiCad"
+  desc "Electronics design automation suite"
+  homepage "https://kicad.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  conflicts_with cask: %w[
+    kicad
+    ddribin-kicad7
+]
+  depends_on macos: ">= :big_sur"
+
+  suite "KiCad"
+  binary "KiCad/KiCad.app/Contents/MacOS/dxf2idf"
+  binary "KiCad/KiCad.app/Contents/MacOS/idf2vrml"
+  binary "KiCad/KiCad.app/Contents/MacOS/idfcyl"
+  binary "KiCad/KiCad.app/Contents/MacOS/idfrect"
+  binary "KiCad/KiCad.app/Contents/MacOS/kicad-cli"
+  artifact "demos", target: "/Library/Application Support/kicad/demos"
+
+  zap trash: [
+    "/Library/Application Support/kicad",
+    "~/Library/Application Support/kicad",
+    "~/Library/Preferences/kicad",
+    "~/Library/Preferences/org.kicad-pcb.*",
+    "~/Library/Saved Application State/org.kicad-pcb.*",
+  ]
+end
