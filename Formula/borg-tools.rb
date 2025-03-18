efea5cdb054178c35684f069a95e37e260d81cd6
Dave Dribin <dave-vcs@dribin.org>
2025-03-18 10:11:32 -0500

    borg-tools: Switch to tagged version scheme, instead of by git hash, starting at version 1.0.0

--- Formula/borg-tools.rb
+++ Formula/borg-tools.rb
@@ -1,10 +1,9 @@
 class BorgTools < Formula
   desc "Dave Dribin's Borg tools"
   homepage "https://github.com/ddribin/borg-tools"
-  url "https://github.com/ddribin/borg-tools.git",
-    using:    :git,
-    revision: "e8e2769e5f1d32ff026d6ee94c9bfb1748e8918e"
-  version "2024011901"
+  url "https://github.com/ddribin/borg-tools/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "18957072e8ad48ec9fa7447b011fcbb268f4f9f50604652153b13bcc93e3e02e"
+  version_scheme 1
   license "MIT"
 
   def install
