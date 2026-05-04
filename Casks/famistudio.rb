10bdc50a82c527b9746f1f3e422b8cb13ca5fbe9
Dave Dribin <dave-vcs@dribin.org>
2026-05-04 09:44:27 -0500

    famistudio: Bump to version 4.5.0

--- Casks/famistudio.rb
+++ Casks/famistudio.rb
@@ -1,6 +1,6 @@
 cask "famistudio" do
-  version "3.2.3"
-  sha256 "716e8aa531d61dfbc7a11eca8cbd61aa359dfa9e079b3522f7dd664629effa33"
+  version "4.5.0"
+  sha256 "e34295a6a80d2ff6dc58f86efa0504509cf6389c87a6a4da15aa67a5ae12a5f4"
 
   url "https://github.com/BleuBleu/FamiStudio/releases/download/#{version}/FamiStudio#{version.no_dividers}-MacOS.zip",
       verified: "github.com/BleuBleu/FamiStudio/"
@@ -8,7 +8,7 @@ cask "famistudio" do
   desc "Simple music editor for the Nintendo Entertainment System or Famicom"
   homepage "https://famistudio.org/"
 
-  depends_on cask: "mono-mdk"
+  depends_on cask: "dotnet-runtime@8"
 
   app "FamiStudio.app"
 end
